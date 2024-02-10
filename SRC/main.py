19c5f4ebb07ca29701ff4ded56aabfedb8bf9cf6
Noum3n <pawel.toroj@icloud.com>
2024-02-10 08:08:03 +0100

    Directory structure. New Project.

---
+++ SRC/main.py
@@ -0,0 +1,5 @@
+
+"""Data_directory - status: excluded. To be used for data storage.
+SRC_directory - status: included. To be used for source code.
+TESTS_directory - status: included. To be used for testing."""
+
